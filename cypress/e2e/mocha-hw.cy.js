5bc59ce49bb7a2c62917e3babc79c2feba084eab
Art <artem.nii@gmail.com>
2023-01-31 11:12:11 +0300

    Parametrized test done against the Block Form

--- cypress/e2e/mocha-hw.cy.js
+++ cypress/e2e/mocha-hw.cy.js
@@ -58,13 +58,16 @@ const dashboardElements = {
 
 
 const testData = [
-  { testData: inputs.block_form.first_name, expectedREsult: inputs.block_form.first_name }
+  { testData: inputs.block_form.first_name, expectedREsult: inputs.block_form.first_name },
+  { testData: inputs.block_form.email, expectedREsult: inputs.block_form.email },
+  { testData: inputs.block_form.last_name, expectedREsult: inputs.block_form.last_name },
+  { testData: inputs.block_form.website, expectedREsult: inputs.block_form.website }
 ]
 
 function clicker(element) {
   cy.get(element).click({ force: true })
   return this
-}  
+}
 /*
 function formWorker(description, locatorsArrayToFill, locatorsArrayToClick) {
   describe(description, () => {
@@ -84,18 +87,25 @@ function formWorker(description, locatorsArrayToFill, locatorsArrayToClick) {
   })
 }
 */
+const myBlockForm = [
+  inputs.block_form.first_name,
+  inputs.block_form.email, 
+  inputs.block_form.last_name,
+  inputs.block_form.website
+]
 
-describe('Parametrized tests', () => {
-  //https://sanitarskyi-ngx-admin.herokuapp.com/pages/forms/layouts
-  testData.forEach(({ testData, expectedREsult }) => {
-    it(`form layout`, () => {
-      cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/')
-      cy.contains('Material Dark').click()
-      clicker(dashboardElements.forms_dropdown)
-      clicker(dashboardElements.forms_dropdown_layouts)
-      cy.get(inputs.block_form.first_name)
-        .type(testData)
-        .should("contain.value", expectedREsult)
-    })
-  })
+describe('Parametrized tests for block form', () => {
+
+    testData.forEach(({ testData, expectedREsult }, i) => {
+      it(`The case for filling out the Block form by value: ${myBlockForm[i]}`, () => {
+        cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/')
+        cy.contains('Material Dark').click()
+        clicker(dashboardElements.forms_dropdown)
+        clicker(dashboardElements.forms_dropdown_layouts)
+
+        cy.get(myBlockForm[i])
+          .type(testData)
+          .should("contain.value", expectedREsult)
+      })
+    })  
 })
