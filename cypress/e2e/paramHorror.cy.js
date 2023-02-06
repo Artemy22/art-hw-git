ca98ace116fdbadfda9ce1318f06560c8bc69dec
Art <artem.nii@gmail.com>
2023-02-07 01:49:06 +0300

    first version done. 2 switch cases implemented. works without physical detection of the toast

--- cypress/e2e/paramHorror.cy.js
+++ cypress/e2e/paramHorror.cy.js
@@ -2,71 +2,145 @@
 ///<reference types="cypress"/>
 
 const elements = {
-  modalOverlaysDropDown: 'menu-item ng-tns-c141-19 ng-star-inserted',
-  optionToastr: 'menu-item ng-tns-c141-23',
-  toasterConfigurationPosition: 'mat-ripple position-select',
-  toasterConfigurationTitle: '[ng-reflect-name="title"]',
-  toasterConfigurationPosition: '[ng-reflect-name="content"]',
-  toasterConfigurationPosition: '[ng-reflect-name="timeout"]',
-
-
+  modalOverlaysDropDown: '[title="Modal & Overlays"]',
+  optionToastr: '[title="Toastr"]',
+  toasterConfigurationPosition: '[ng-reflect-selected="top-right"]',
+  toasterConfigurationTypeDropdown:
+  {
+    main: ':nth-child(2) > .form-group > .mat-ripple > .select-button',
+    primary: '[ng-reflect-value="primary"]',
+    success: '[ng-reflect-value="success"]',
+    warning: '[ng-reflect-value="warning"]',
+    danger: '[ng-reflect-value="danger"]'
+  },
+  positionPopup: {
+    bottomStart: '[id="cdk-overlay-1"]',  //primary   #6200ee   rgb(98, 0, 238)    cdk-overlay-container.eq1
+    topStart: '[id="cdk-overlay-2"]',     //success   #60af20   rgb(96, 175, 32)
+    bottomRight: '[id="cdk-overlay-4"]',  //warning   #ff9f05   rgb(255, 159, 5)
+    topRight: '[id="cdk-overlay-5"]'      //danger    #b00020   rgb(176, 0, 32)
+  },
+  positionInDropdown: {
+    bottomStart: '[id="nb-option-31"]',  //primary   #6200ee   rgb(98, 0, 238)    cdk-overlay-container.eq1
+    topStart: '[id="nb-option-29"]',     //success   #60af20   rgb(96, 175, 32)
+    bottomRight: '[id="nb-option-27"]',  //warning   #ff9f05   rgb(255, 159, 5)
+    topRight: '[id="nb-option-24"]'      //danger    #b00020   rgb(176, 0, 32)
+  },
+  colors: {
+    bottomStart: 'rgb(233, 29, 99)',
+    topStart: 'rgb(96, 175, 32)',
+    bottomRight: ' rgb(255, 159, 5)',
+    topRight: '  rgb(176, 0, 32)',
+  },
+  toasterConfigurationTitle: '[name="title"]',
+  toasterConfigurationContent: '[ng-reflect-name="content"]',
+  toasterConfigurationtimeout: '[ng-reflect-name="timeout"]',
+  mainDivToCheck: 'cdk-overlay-container',
+  burgerButton: '[data-name="menu"]',
+  showToastButton: 'nb-card-footer .status-basic'
 }
 
-const dashboardElements = {
-  forms_dropdown: 'a[title="Forms"]',
-  forms_dropdown_layouts: 'a[title="Form Layouts"]'
-}
-
-
-const testData = [
-  { testData: inputs.block_form.first_name, expectedREsult: inputs.block_form.first_name },
-  { testData: inputs.block_form.email, expectedREsult: inputs.block_form.email },
-  { testData: inputs.block_form.last_name, expectedREsult: inputs.block_form.last_name },
-  { testData: inputs.block_form.website, expectedREsult: inputs.block_form.website }
-]
-
-function clicker(element) {
-  cy.get(element).click({ force: true })
-  return this
-}
 /*
-function formWorker(description, locatorsArrayToFill, locatorsArrayToClick) {
-  describe(description, () => {
-    it('passes', () => {
-      cy.visit(urlToGo)
+cy.get('cdk-overlay-container')
+.eq(1)
+.should('have.css', 'background-color')
+.and('eq', 'rgb(98, 0, 238)')
 
-      goToNeededPageFlow()
+cy.get('cdk-overlay-container')
+.eq(2)
+.should('have.css', 'background-color')
+.and('eq', 'rgb(96, 175, 32)')
 
-      for (let i = 0; i < locatorsArrayToFill.length; i++) {
-        cy.get(locatorsArrayToFill[i]).type(description)
-      }
+cy.get('cdk-overlay-container')
+.eq(3)
+.should('have.css', 'background-color')
+.and('eq', 'rgb(255, 159, 5)')
 
-      for (let j = 0; j < locatorsArrayToClick.length; j++) {
-        clicker(locatorsArrayToClick[j])
-      }
-    })
-  })
-}
+cy.get('cdk-overlay-container')
+.eq(4)
+.should('have.css', 'background-color')
+.and('eq', 'rgb(176, 0, 32)')
 */
-const myBlockForm = [
-  inputs.block_form.first_name,
-  inputs.block_form.email, 
-  inputs.block_form.last_name,
-  inputs.block_form.website
+
+
+const testData = {
+  position: 'top-right',
+  title: 'test title',
+  content: 'test content',
+  time: '1000',
+  type: 'primary'
+}
+
+const testDataArr = [
+  { testData: elements.positionInDropdown.bottomStart, expectedResult: elements.colors.bottomStart },
+  { testData: elements.positionInDropdown.topStart, expectedResult: elements.colors.topStart },
+  { testData: elements.positionInDropdown.bottomRight, expectedResult: elements.colors.bottomRight },
+  { testData: elements.positionInDropdown.topRight, expectedResult: elements.colors.topRight },
 ]
 
-describe('Parametrized tests for toastr', () => {
-
-    testData.forEach(({ testData, expectedREsult }, i) => {
-      it(`The case for filling out the Block form by value: ${myBlockForm[i]}`, () => {
-        cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/')
-        cy.contains('Material Dark').click()
-        clicker(dashboardElements.forms_dropdown)
-        clicker(dashboardElements.forms_dropdown_layouts)
-
-        cy.get(myBlockForm[i])
-          .type(testData)
-          .should("contain.value", expectedREsult)
-      })
-    })  
+beforeEach('', () => {
+  cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/')
+  cy.contains('Material Dark').click()
+  cy.get(elements.modalOverlaysDropDown).click()
+  cy.get(elements.optionToastr).click()
+  cy.get(elements.burgerButton).click()
 })
+
+testDataArr.forEach(({ testData, expectedResult }) => {
+  it(`Param horror and pain`, () => {
+    cy.get(elements.toasterConfigurationPosition).click()
+    cy.get(testData).click()
+    cy.get(elements.toasterConfigurationTitle).type(`${testData}`).should('contain.value', `${testData}`)
+    cy.get(elements.toasterConfigurationContent).type(`${testData}`).should('contain.value', `${testData}`)
+    cy.get(elements.toasterConfigurationtimeout).type(20000).should('contain.value', 2000)
+
+    cy.log(expectedResult)
+    switch (testData) {
+      case elements.positionInDropdown.bottomStart:
+        cy.get(elements.toasterConfigurationTypeDropdown.main).click()
+        cy.get(elements.toasterConfigurationTypeDropdown.primary).click()
+        cy.get(elements.showToastButton).click()
+        cy.get('.ng-tns-c209-54.status-primary')          
+        .should('have.css', 'background-color')
+          .and('eq', expectedResult)
+        break;
+
+        case elements.positionInDropdown.topStart:
+          cy.get(elements.toasterConfigurationTypeDropdown.main).click()
+          cy.get(elements.toasterConfigurationTypeDropdown.success).click()
+          cy.get(elements.showToastButton).click()
+          cy.get('.ng-tns-c209-54.status-success')          
+          .should('have.css', 'background-color')
+            .and('eq', expectedResult)
+          break;
+
+      default:
+        break;
+    }
+
+
+  })
+})
+
+/*
+describe('Parametrized tests for toastr', () => {
+  testData.forEach(({ testData, expectedResult }, i) => {
+    it(`The case for filling out the Block form by value`, () => {
+      cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/')
+      cy.contains('Material Dark').click()
+      cy.get(elements.modalOverlaysDropDown).click()
+      cy.get(elements.optionToastr).click()
+
+      cy.get(elements.toasterConfigurationTitle)
+        .type(testData)
+        .should("contain.value", expectedResult)
+    })
+  }) 
+
+
+  it('', () => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/')
+    cy.contains('Material Dark').click()
+    cy.get(elements.modalOverlaysDropDown)
+    cy.get(elements.optionToastr)
+  })
+})*/
