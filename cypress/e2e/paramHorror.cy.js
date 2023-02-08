35bad7acc49aa23809c616046206b694f3d1896a
Art <artem.nii@gmail.com>
2023-02-09 01:09:40 +0300

    test for 1 case done

--- cypress/e2e/paramHorror.cy.js
+++ cypress/e2e/paramHorror.cy.js
@@ -46,29 +46,41 @@ const toasts = {
   topRight: '.ng-tns-c209-54.status-danger'
 }
 
-const testDataArr = [
-  { testData: elements.positionInDropdown.bottomStart, expectedResult: elements.colors.bottomStart },
-  { testData: elements.positionInDropdown.topStart, expectedResult: elements.colors.topStart },
-  { testData: elements.positionInDropdown.bottomRight, expectedResult: elements.colors.bottomRight },
-  { testData: elements.positionInDropdown.topRight, expectedResult: elements.colors.topRight },
-]
+const objc = {
+  position: elements.positionInDropdown.bottomStart,
+  title: 'bottom start toast',
+  content: 'bottom start content',
+  timeout: 1000,
+  type: elements.toasterConfigurationTypeDropdown.primary,
+  status: 'primary',
+  color: elements.colors.bottomStart
+}
 
-beforeEach('', () => {
+it('Param horror and pain of tost#', () => {
   cy.navigateToDashboard()
   cy.get(elements.modalOverlaysDropDown).click()
-    cy.get(elements.optionToastr).click()
-    cy.get(elements.burgerButton).click()
+  cy.get(elements.optionToastr).click()
+  cy.get(elements.burgerButton).click()
+  cy.get(elements.toasterConfigurationPosition).click()
+  cy.get(Object.values(objc)[0]).click() // position of the toast
+  cy.inputFiller(elements.toasterConfigurationTitle, `title ${Object.values(objc)[1]}`)
+  cy.inputFiller(elements.toasterConfigurationContent, `content ${Object.values(objc)[2]}`)
+  cy.inputFiller(elements.toasterConfigurationtimeout, `${Object.values(objc)[3]}`)
+  cy.get(elements.toasterConfigurationTypeDropdown.main).click()
+  cy.get(Object.values(objc)[4]).click() //  type e.x. primary
+  cy.get(elements.showToastButton).click()
+
+  cy.get(`.toastr-overlay-container .status-${Object.values(objc)[5]}`)
+    .should('exist')
+    .and('have.css', 'background-color')
+    .and('eq', Object.values(objc)[6]) // color rgb
+
+  //.toastr-overlay-container .status-success
 })
 
-testDataArr.forEach(({ testData, expectedResult }, i) => {
-  it(`Param horror and pain of tost#${i}`, () => {
-    cy.get(elements.toasterConfigurationPosition).click()
-    cy.get(testData).click()
-    cy.inputFiller(elements.toasterConfigurationTitle,testData)
-    cy.inputFiller(elements.toasterConfigurationContent, testData)
-    cy.inputFiller(elements.toasterConfigurationtimeout, 20000)
 
-    switch (testData) {
+/**
+     switch (testData) {
       case elements.positionInDropdown.bottomStart:
         cy.get(elements.toasterConfigurationTypeDropdown.main).click()
         cy.get(elements.toasterConfigurationTypeDropdown.primary).click()
@@ -115,5 +127,140 @@ testDataArr.forEach(({ testData, expectedResult }, i) => {
       default:
         break;
     }
-  })
+ */
+
+
+
+/*
+ 
+
+///<reference types="cypress"/>
+
+const elements = {
+modalOverlaysDropDown: '[title="Modal & Overlays"]',
+optionToastr: '[title="Toastr"]',
+toasterConfigurationPosition: '[ng-reflect-selected="top-right"]',
+toasterConfigurationTypeDropdown:
+{
+main: ':nth-child(2) > .form-group > .mat-ripple > .select-button',
+primary: '[ng-reflect-value="primary"]',
+success: '[ng-reflect-value="success"]',
+warning: '[ng-reflect-value="warning"]',
+danger: '[ng-reflect-value="danger"]'
+},
+positionPopup: {
+bottomStart: '[id="cdk-overlay-1"]',  //primary   #6200ee   rgb(98, 0, 238)    cdk-overlay-container.eq1
+topStart: '[id="cdk-overlay-2"]',     //success   #60af20   rgb(96, 175, 32)
+bottomRight: '[id="cdk-overlay-4"]',  //warning   #ff9f05   rgb(255, 159, 5)
+topRight: '[id="cdk-overlay-5"]'      //danger    #b00020   rgb(176, 0, 32)
+},
+positionInDropdown: {
+bottomStart: '[id="nb-option-31"]',  //primary   #6200ee   rgb(98, 0, 238)    cdk-overlay-container.eq1
+topStart: '[id="nb-option-29"]',     //success   #60af20   rgb(96, 175, 32)
+bottomRight: '[id="nb-option-27"]',  //warning   #ff9f05   rgb(255, 159, 5)
+topRight: '[id="nb-option-24"]'      //danger    #b00020   rgb(176, 0, 32)
+},
+colors: {
+bottomStart: 'rgb(233, 29, 99)',
+topStart: 'rgb(96, 175, 32)',
+bottomRight: 'rgb(255, 159, 5)',
+topRight: 'rgb(176, 0, 32)',
+},
+toasterConfigurationTitle: '[name="title"]',
+toasterConfigurationContent: '[ng-reflect-name="content"]',
+toasterConfigurationtimeout: '[ng-reflect-name="timeout"]',
+mainDivToCheck: 'cdk-overlay-container',
+burgerButton: '[data-name="menu"]',
+showToastButton: 'nb-card-footer .status-basic'
+}
+
+const toasts = {
+bottomStart: '.ng-tns-c209-54.status-primary',
+topStart: '.ng-tns-c209-54.status-success',
+bottomRight: '.ng-tns-c209-54.status-warning',
+topRight: '.ng-tns-c209-54.status-danger'
+}
+/*
+const testDataArr = [
+{ testData: elements.positionInDropdown.bottomStart, expectedResult: elements.colors.bottomStart },
+{ testData: elements.positionInDropdown.topStart, expectedResult: elements.colors.topStart },
+{ testData: elements.positionInDropdown.bottomRight, expectedResult: elements.colors.bottomRight },
+{ testData: elements.positionInDropdown.topRight, expectedResult: elements.colors.topRight },
+]
+
+const testDataArr = [
+{
+position: elements.positionInDropdown.bottomStart,
+title: 'test title',
+content: 'test content',
+time: '20000',
+type: 'primary'
+},
+]
+
+beforeEach('', () => {
+cy.navigateToDashboard()
+cy.get(elements.modalOverlaysDropDown).click()
+cy.get(elements.optionToastr).click()
+cy.get(elements.burgerButton).click()
 })
+
+testDataArr.forEach(({ testData, expectedResult }, i) => {
+it(`Param horror and pain of tost#${i}`, () => {
+cy.get(elements.toasterConfigurationPosition).click()
+cy.get(testData).click()
+cy.inputFiller(elements.toasterConfigurationTitle, testData)
+cy.inputFiller(elements.toasterConfigurationContent, testData)
+cy.inputFiller(elements.toasterConfigurationtimeout, 20000)
+})
+})
+
+ switch (testData) {
+  case elements.positionInDropdown.bottomStart:
+    cy.get(elements.toasterConfigurationTypeDropdown.main).click()
+    cy.get(elements.toasterConfigurationTypeDropdown.primary).click()
+    cy.get(elements.showToastButton).click()
+    cy.get(toasts.bottomStart)
+      .should('have.css', 'background-color')
+      .and('eq', expectedResult)
+    cy.get(toasts.bottomStart).click('bottomLeft', { force: true })
+    cy.get(toasts.bottomStart).should('not.exist')
+    break;
+
+  case elements.positionInDropdown.topStart:
+    cy.get(elements.toasterConfigurationTypeDropdown.main).click()
+    cy.get(elements.toasterConfigurationTypeDropdown.success).click()
+    cy.get(elements.showToastButton).click()
+    cy.get(toasts.topStart)
+      .should('have.css', 'background-color')
+      .and('eq', expectedResult)
+    cy.get(toasts.topStart).click('topLeft', { force: true })
+    cy.get(toasts.topStart).should('not.exist')
+    break;
+
+  case elements.positionInDropdown.bottomRight:
+    cy.get(elements.toasterConfigurationTypeDropdown.main).click()
+    cy.get(elements.toasterConfigurationTypeDropdown.warning).click()
+    cy.get(elements.showToastButton).click()
+    cy.get(toasts.bottomRight)
+      .should('have.css', 'background-color')
+      .and('eq', expectedResult)
+    cy.get(toasts.bottomRight).click('bottomRight', { force: true })
+    cy.get(toasts.bottomRight).should('not.exist')
+    break;
+
+  case elements.positionInDropdown.topRight:
+    cy.get(elements.toasterConfigurationTypeDropdown.main).click()
+    cy.get(elements.toasterConfigurationTypeDropdown.danger).click()
+    cy.get(elements.showToastButton).click()
+    cy.get(toasts.topRight)
+      .should('have.css', 'background-color')
+      .and('eq', expectedResult)
+    cy.get(toasts.topRight).click('topRight', { force: true })
+    cy.get(toasts.topRight).should('not.exist')
+    break;
+  default:
+    break;
+}
+*/
+
